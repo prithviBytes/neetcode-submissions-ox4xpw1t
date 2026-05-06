4eab038b50c76877f13aff04bfb8b5cb418432c1
Prithvi Suvarna <prithvisuvarna56@gmail.com>
2026-05-06 20:47:14 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.js
@@ -0,0 +1,32 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        if(!node) return null;
+        let map = new Map();
+        function dfs(curr) {
+            if(map.has(curr)) {
+                return map.get(curr);
+            }
+            let clone = new Node(curr.val);
+            map.set(curr, clone);
+            for(let n of curr.neighbors) {
+                clone.neighbors.push(dfs(n))
+            }
+            return clone;
+        }
+        return dfs(node);
+    }
+}
