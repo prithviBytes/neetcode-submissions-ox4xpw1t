965b72388f1ebc8c73b7465bceff2d740e031222
Prithvi Suvarna <prithvisuvarna56@gmail.com>
2026-08-19 18:11:17 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums = nums.sort((a, b) => a - b)
+        let results = []
+        for(let i = 0; i < nums.length - 2; i++) {
+            if(nums[i] > 0 || nums[i] === nums[i - 1]) continue;
+            let target = -1 * (nums[i]);
+            let left = i + 1;
+            let right = nums.length - 1;
+            while(left < right) {
+                let currentSum = nums[left] + nums[right];
+                if(currentSum > target) {
+                    right--;
+                } else if(currentSum < target) {
+                    left++
+                } else {
+                    results.push([nums[i], nums[left], nums[right]]);
+                    while(nums[left] === nums[left + 1])left++;
+                    while(nums[right] === nums[right - 1])right--;
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return results;
+    }
+}
